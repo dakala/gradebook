f0ed62e9fb65098d07729704a36f2f174d94c56f
Deji Akala <dejiakala@gmail.com>
2016-01-01 17:40:28 +0000

    Remove view call

--- modules/grade_scale/src/Controller/GradeScaleViewController.php
+++ modules/grade_scale/src/Controller/GradeScaleViewController.php
@@ -16,37 +16,6 @@ use Drupal\Core\Entity\Controller\EntityViewController;
 class GradeScaleViewController extends EntityViewController {
 
   /**
-   * {@inheritdoc}
-   */
-  public function view(EntityInterface $grade_scale, $view_mode = 'full', $langcode = NULL) {
-    $build = parent::view($grade_scale, $view_mode, $langcode);
-
-    foreach ($grade_scale->uriRelationships() as $rel) {
-      // Set the node path as the canonical URL to prevent duplicate content.
-      $build['#attached']['html_head_link'][] = array(
-        array(
-          'rel' => $rel,
-          'href' => $grade_scale->url($rel),
-        ),
-        TRUE,
-      );
-
-      if ($rel == 'canonical') {
-        // Set the non-aliased canonical path as a default shortlink.
-        $build['#attached']['html_head_link'][] = array(
-          array(
-            'rel' => 'shortlink',
-            'href' => $grade_scale->url($rel, array('alias' => TRUE)),
-          ),
-          TRUE,
-        );
-      }
-    }
-
-    return $build;
-  }
-
-  /**
    * The _title_callback for the page that renders a single node.
    *
    * @param \Drupal\Core\Entity\EntityInterface $grade_scale
